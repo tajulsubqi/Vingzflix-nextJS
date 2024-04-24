06a7d414a4a4ed397ec0e2e7621f720a8361194b
Tajul <subqitajul7@gmail.com>
2024-04-24 21:59:57 +0700

    feat: add page login and sign-up

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import React from "react"
+import Background from "../../../public/login_background.jpg"
+import Logo from "../../../public/netflix_logo.svg"
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="relative flex h-screen w-full flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Image
+        src={Background}
+        alt="background"
+        className="hidden sm:flex sm:object-cover -z-10 brightness-50"
+        priority
+        fill
+      />
+
+      <Image
+        src={Logo}
+        alt="logo"
+        width={120}
+        height={120}
+        priority
+        className="absolute left-4 top-4 object-contain md:left-10 md:top-6"
+      />
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,52 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { GithubIcon } from "lucide-react"
+import Link from "next/link"
+import React from "react"
+import GoogleIcon from "../../../../public/google.svg"
+import Image from "next/image"
+
+const Register = () => {
+  return (
+    <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+      <form action="">
+        <h1 className="text-3xl font-semibold text-white">Sign Up</h1>
+        <div className="space-y-4 mt-5">
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="w-full bg-[#333] placeholder:text-xs placeholder:text-gray-400 inline-block"
+          />
+          {/* <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="w-full bg-[#333] placeholder:text-xs placeholder:text-gray-400 inline-block"
+          /> */}
+          <Button type="submit" variant={"destructive"} className="w-full bg-[#e50914]">
+            Sign Up
+          </Button>
+        </div>
+      </form>
+
+      <div className="text-sm text-gray-400 mt-2">
+        Already Have a account?
+        <Link href="/login" className="ml-1 text-white hover:underline">
+          Log in Now!
+        </Link>
+      </div>
+
+      <div className="flex w-full justify-center items-center gap-x-6">
+        <Button variant={"outline"} className="w-1/5 mt-4 gap-2">
+          <GithubIcon className="w-5 h-5" />
+        </Button>
+        <Button variant={"outline"} className="w-1/5 mt-4 gap-2">
+          <Image src={GoogleIcon} alt="google" className="w-7" />
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default Register
